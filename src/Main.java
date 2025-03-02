04ab64053b028f66c40924c088e92e6e589716c0
Pushkar-Mahajan4 <pushkarmahajan4@gmail.com>
2025-03-01 22:15:07 -0500

    Fix byte reading flaws

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,16 +10,18 @@ class Main {
     public static List<Byte> loadDataset(String fileLocation) throws Exception {
 
         try {
-            FileReader fileReader = new FileReader(fileLocation);
-            byte read;
+            // Use FileInputStream to read raw bytes, FileReader reads
+            FileInputStream fileReader = new FileInputStream (fileLocation);
+            int read;
 
             List<Byte> byteList = new ArrayList<>();
-            while ((read = (byte) fileReader.read()) != -1) {
-                byteList.add(read);
+            while ((read = fileReader.read()) != -1) {
+                byteList.add((byte) read);
             }
 
+            fileReader.close();
             /* Data is stored in Big endian format (Most significant bit - stored first) */
-            int magicNumber = Util.getIntFromBytes(byteList.subList(0, 5));
+            int magicNumber = Util.getIntFromBytes(byteList.subList(0, 4));
             if(magicNumber == 2051) {
                 System.out.println("Reading image file..");
             } else if(magicNumber == 2049) {
@@ -46,6 +48,8 @@ class Main {
 
         /* Load the dataset */
         List<Byte> trainImages = loadDataset(trainImagesLocation);
+        System.out.println("trainImages size : " +trainImages.size());
+
         List<Byte> trainLabels = loadDataset(trainLabelsLocation);
         List<Byte> testImages = loadDataset(testImagesLocation);
         List<Byte> testLabels = loadDataset(testLabelsLocation);
@@ -53,47 +57,39 @@ class Main {
         if (trainImages == null || trainLabels == null || testImages == null || testLabels == null) {
             System.out.println("Problem occurred during loading of datasets");
         }
+
+        // Verify 60000 train images
         int numberOfTrainImages = Util.getIntFromBytes(trainImages.subList(4, 8));
+        Util.assertNumberOfTrainImages(numberOfTrainImages);
 
         // Verify that rows & column are 28
         int numberOfRows = Util.getIntFromBytes(trainImages.subList(8, 12));
         int numberOfColumns = Util.getIntFromBytes(trainImages.subList(12, 16));
-        try {
-            Util.assertRowsAndColumns(numberOfRows, numberOfColumns);
-        } catch (AssertionError e) {
-            System.out.println("Expected 28 row x 28 columns image");
-            System.exit(0);
-        }
+        Util.assertRowsAndColumns(numberOfRows, numberOfColumns);
 
-        System.out.println("Size : " +trainImages.size());
-        System.out.println("numberOfTrainImages : " +numberOfTrainImages);
-
-        /* Load each image */
+//        /* Load each image */
         List<int[][]> eachImage = Util.getEachImage(trainImages);
 
-        /* Uncomment to view the first image */
-//        displayImage(eachImage.getFirst());
-//        eachImage.add(Util.getImagePixels(trainImages, 16));
+        /* Load each label */
+//        List<int[]> eachLabel = Util.getEachLabel(trainLabels);
 
         /* Normalizing pixel values to 0 - 1, check docs to see why float */
-        List<float[][]> normalizedPixels = NeuralNWUtils.normalizePixels(eachImage);
+//        List<float[][]> normalizedPixels = NeuralNWUtils.normalizePixels(eachImage);
 
-
+        /* Uncomment to view the first image */
+        // 12 -> 3, 8 -> 1
+        displayImage(eachImage.get(90));
 
     }
 
     public static void displayImage(int[][] input) {
-        /* Java's BufferedImage, RGB values are stored as a single 32-bit integer
-        * Bits 16-23 represent red (R)
-        * Bits 8-15 represent green (G)
-        * Bits 0-7 represent blue (B) */
+        /* Java's BufferedImage, RGB values are stored as a single 32-bit integer */
         BufferedImage image = new BufferedImage(28, 28, BufferedImage.TYPE_BYTE_GRAY);
         for(int y = 0; y < 28; y++) {
             for(int x = 0; x < 28; x++) {
                 int pixelValue = input[y][x];
-                // The bit shifting operation is constructing an RGB integer
-                int rgb = (pixelValue << 16) | (pixelValue << 8) | pixelValue;
-                image.setRGB(x, y, rgb);
+                System.out.println("PixelValue : " + pixelValue);
+                image.getRaster().setSample(x, y, 0, pixelValue);
             }
         }
 
