cd5c8ae16696162e8261f40a536425d3a984edbd
Pushkar-Mahajan4 <pushkarmahajan4@gmail.com>
2025-03-04 21:03:29 -0500

    Test model's accuracy

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ class Main {
 
         try {
             // Use FileInputStream to read raw bytes, FileReader reads
-            FileInputStream fileReader = new FileInputStream (fileLocation);
+            FileInputStream fileReader = new FileInputStream(fileLocation);
             int read;
 
             List<Byte> byteList = new ArrayList<>();
@@ -22,16 +22,16 @@ class Main {
             fileReader.close();
             /* Data is stored in Big endian format (Most significant bit - stored first) */
             int magicNumber = Util.getIntFromBytes(byteList.subList(0, 4));
-            if(magicNumber == 2051) {
+            if (magicNumber == 2051) {
                 System.out.println("Reading image file..");
-            } else if(magicNumber == 2049) {
+            } else if (magicNumber == 2049) {
                 System.out.println("Reading label file..");
             } else {
                 throw new IllegalArgumentException("Invalid File format received");
             }
 
             return byteList;
-        } catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
             return null;
         }
@@ -51,38 +51,50 @@ class Main {
         List<Byte> trainLabels = loadDataset(trainLabelsLocation);
         List<Byte> testImages = loadDataset(testImagesLocation);
         List<Byte> testLabels = loadDataset(testLabelsLocation);
+
+        /* Validate images */
         AssertionUtils.validateTrainImages(trainImages);
-//        /* Load each image */
-        List<int[][]> eachImage = Util.getEachImage(trainImages);
+        AssertionUtils.validateTrainImages(testImages);
+
+        /* Load each train image */
+        List<int[][]> loadTrainImages = Util.getEachImage(trainImages);
+
+        /* Load each train image */
+        List<int[][]> loadTestImages = Util.getEachImage(testImages);
 
         /* Uncomment to view the first image */
-//        displayImage(eachImage.get(90));
+        // displayImage(eachImage.get(90));
 
         /* Load each label */
-        List<Integer> extractedLabels = Util.extractLabels(trainLabels, 8);
+        List<Integer> extractedTrainLabels = Util.extractLabels(trainLabels, 8);
+        List<Integer> extractedTestLabels = Util.extractLabels(testLabels, 8);
         AssertionUtils.validateTrainLabels(trainLabels);
+        AssertionUtils.validateTestLabels(testLabels);
 
         /* Uncomment to go through every label */
-//        for(Integer i : eachLabel) {
-//            System.out.println("Label : " + i);
-//        }
+        // for(Integer i : eachLabel) {
+        // System.out.println("Label : " + i);
+        // }
 
         /* Normalizing pixel values to 0 - 1, check docs to see why float */
-        List<float[][]> normalizedPixels = NeuralNWUtils.normalizePixels(eachImage);
+        List<float[][]> normalizedTrainPixels = NeuralNWUtils.normalizePixels(loadTrainImages);
+        List<float[][]> normalizedTestPixels = NeuralNWUtils.normalizePixels(loadTestImages);
 
         /* One hot encode training labels */
-        List<int[]> encodedTrainLabels = Util.encodeLabels(extractedLabels);
+        List<int[]> encodedTrainLabels = Util.encodeLabels(extractedTrainLabels);
+        List<int[]> encodedTestLabels = Util.encodeLabels(extractedTestLabels);
 
         /* Train the network */
         System.out.println("Training neural network..");
-        NeuralNetwork trainedNetwork = new NeuralNetwork(normalizedPixels, encodedTrainLabels);
+        NeuralNetwork trainedNetwork = new NeuralNetwork(normalizedTrainPixels, encodedTrainLabels,
+                normalizedTestPixels, encodedTestLabels);
     }
 
     public static void displayImage(int[][] input) {
         /* Java's BufferedImage, RGB values are stored as a single 32-bit integer */
         BufferedImage image = new BufferedImage(28, 28, BufferedImage.TYPE_BYTE_GRAY);
-        for(int y = 0; y < 28; y++) {
-            for(int x = 0; x < 28; x++) {
+        for (int y = 0; y < 28; y++) {
+            for (int x = 0; x < 28; x++) {
                 int pixelValue = input[y][x];
                 System.out.println("PixelValue : " + pixelValue);
                 image.getRaster().setSample(x, y, 0, pixelValue);
